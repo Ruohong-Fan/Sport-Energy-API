e102a5b1500e2d47569b974e907c87a27de16d27
Ruohong-Fan <ruohong.fan@gmail.com>
2018-05-19 14:12:08 +0800

    Transaction API v1

--- api/controllers/sportEnergyAccountController.js
+++ api/controllers/sportEnergyAccountController.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient,
-  url = "mongodb://localhost:27017/";
+  url = 'mongodb://localhost:27017/';
 
 exports.read_sportEnergyAccount = function(req, res) {
   //Search by name and profile
@@ -7,9 +7,9 @@ exports.read_sportEnergyAccount = function(req, res) {
     console.log('Read sport energy account by card number');
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-      var dbo = db.db("sportEnergyDB");
+      var dbo = db.db('sportEnergyDB');
       var whereStr = {'cardNumber':req.query.cardNumber};
-      dbo.collection("sportEnergyAccount").find(whereStr).toArray(function(err, sportEnergyAccount) {
+      dbo.collection('sportEnergyAccount').find(whereStr).toArray(function(err, sportEnergyAccount) {
         if (err) throw err;
         res.json(sportEnergyAccount);
         db.close();
@@ -21,8 +21,8 @@ exports.read_sportEnergyAccount = function(req, res) {
     console.log('Read all sport energy account');
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-      var dbo = db.db("sportEnergyDB");
-      dbo.collection("sportEnergyAccount").find({}).toArray(function(err, sportEnergyAccount) {
+      var dbo = db.db('sportEnergyDB');
+      dbo.collection('sportEnergyAccount').find({}).toArray(function(err, sportEnergyAccount) {
         if (err) throw err;
         res.json(sportEnergyAccount);
         db.close();
@@ -32,18 +32,24 @@ exports.read_sportEnergyAccount = function(req, res) {
 };
 
 exports.create_sportEnergyAccount = function(req, res) {
-  console.log('Create sport energy account');
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db('sportEnergyDB');
-    var myobj = {'cardNumber':req.body.cardNumber, 'energyBalance': 0};
-    console.log(myobj);
-    dbo.collection('sportEnergyAccount').insert(myobj, function(err, sportEnergyAccount) {
-        if (err) throw err;
-        res.json(sportEnergyAccount);
-        db.close();
+  if (req.body.cardNumber) {
+    console.log('Create sport energy account');
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db('sportEnergyDB');
+      var myDate = new Date();
+      var myobj = {'cardNumber':req.body.cardNumber, 'energyBalance': 0, 'createTime':myDate.toLocaleString( ), 'updateTime':myDate.toLocaleString( )};
+      console.log(myobj);
+      dbo.collection('sportEnergyAccount').insert(myobj, function(err, sportEnergyAccount) {
+          if (err) throw err;
+          res.json(sportEnergyAccount);
+          db.close();
+      });
     });
-  });
+  }
+  else {
+    res.json({Message: 'Not sufficient information to create sport energy account.'});
+  }
 };
 
 exports.delete_sportEnergyAccount = function(req, res) {
@@ -51,7 +57,7 @@ exports.delete_sportEnergyAccount = function(req, res) {
     console.log('Delete sport energy account by card number');
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-      var dbo = db.db("sportEnergyDB");
+      var dbo = db.db('sportEnergyDB');
       var whereStr = {'cardNumber':req.query.cardNumber};
       dbo.collection('sportEnergyAccount').deleteOne(whereStr, function(err, sportEnergyAccount) {
         if (err) throw err;
@@ -66,21 +72,22 @@ exports.delete_sportEnergyAccount = function(req, res) {
 };
 
 exports.update_sportEnergyAccount = function(req, res) {
-  if (req.query.cardNumber) {
+  if (req.query.cardNumber && req.body.energyBalance) {
     console.log('Update sport energy account by card number');
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db('sportEnergyDB');
-    var whereStr = {'cardNumber':req.query.cardNumber};
-    var updateStr = {$set:req.body};
-    dbo.collection('sportEnergyAccount').updateOne(whereStr, updateStr, function(err, sportEnergyAccount) {
-        if (err) throw err;
-        res.json(sportEnergyAccount);
-        db.close();
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db('sportEnergyDB');
+      var myDate = new Date();
+      var whereStr = {'cardNumber':req.query.cardNumber};
+      var updateStr = {$set:{'energyBalance':req.body.energyBalance, 'updateTime':myDate.toLocaleString( )}};
+      dbo.collection('sportEnergyAccount').updateOne(whereStr, updateStr, function(err, sportEnergyAccount) {
+          if (err) throw err;
+          res.json(sportEnergyAccount);
+          db.close();
+      });
     });
-});
   }
   else {
-    res.json({Message: 'Please provide necessary information to update sport leader.'})
+    res.json({Message: 'Please provide necessary information to update sport energy account.'})
   }
 };
--- api/controllers/sportEnergyTransactionController.js
+++ api/controllers/sportEnergyTransactionController.js
@@ -1,58 +1,30 @@
 var MongoClient = require('mongodb').MongoClient,
-  url = "mongodb://localhost:27017/";
+  url = 'mongodb://localhost:27017/';
 
 exports.read_sportEnergyTransaction = function(req, res) {
-  //Search by name and profile
-  if (req.query.name && req.query.profile) {
-    console.log('Read sport leader by name & profile');
+  //Search by card number
+  if (req.query.cardNumber) {
+    console.log('Read sport energy transaction');
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-      var dbo = db.db("sportLeaderDB");
-      var whereStr = {'name':req.query.name, 'profile':req.query.profile};
-      dbo.collection("sportLeader").find(whereStr).toArray(function(err, sportLeader) {
+      var dbo = db.db('sportEnergyDB');
+      var whereStr = {'cardNumber':req.query.cardNumber};
+      dbo.collection('sportEnergyTransaction').find(whereStr).toArray(function(err, sportEnergyTransaction) {
         if (err) throw err;
-        res.json(sportLeader);
-        db.close();
-      });
-    });
-  }
-  //Search by name
-  else if (req.query.name) {
-    console.log('Read sport leader by name')
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("sportLeaderDB");
-      var whereStr = {'name':req.query.name};
-      dbo.collection("sportLeader").find(whereStr).toArray(function(err, sportLeader) {
-        if (err) throw err;
-        res.json(sportLeader);
-        db.close();
-      });
-    });
-  }
-  //Search by profile
-  else if (req.query.profile) {
-    console.log('Read sport leader by profile')
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("sportLeaderDB");
-      var whereStr = {'profile':req.query.profile};
-      dbo.collection("sportLeader").find(whereStr).toArray(function(err, sportLeader) {
-        if (err) throw err;
-        res.json(sportLeader);
+        res.json(sportEnergyTransaction);
         db.close();
       });
     });
   }
   //List all
   else {
-    console.log('Read all sport leaders');
+    console.log('Read all sport energy transactions');
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-      var dbo = db.db("sportLeaderDB");
-      dbo.collection("sportLeader").find({}).toArray(function(err, sportLeader) {
+      var dbo = db.db('sportEnergyDB');
+      dbo.collection('sportEnergyTransaction').find({}).toArray(function(err, sportEnergyTransaction) {
         if (err) throw err;
-        res.json(sportLeader);
+        res.json(sportEnergyTransaction);
         db.close();
       });
     });
@@ -60,29 +32,43 @@ exports.read_sportEnergyTransaction = function(req, res) {
 };
 
 exports.create_sportEnergyTransaction = function(req, res) {
-  console.log('Create sport leader');
+  console.log('Create sport energy transaction');
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    var dbo = db.db('sportLeaderDB');
-    var myobj = req.body;
-    dbo.collection('sportLeader').insert(myobj, function(err, sportLeader) {
+    var dbo = db.db('sportEnergyDB');
+    // create sport energy transaction
+    var myDate = new Date();
+    var myobj = {'cardNumber':req.body.cardNumber, 'energyUpdate': req.body.energyUpdate, 'createTime':myDate.toLocaleString( ), 'updateTime':myDate.toLocaleString( )};
+    dbo.collection('sportEnergyTransaction').insert(myobj, function(err, sportEnergyTransaction) {
+      if (err) throw err;
+      res.json(sportEnergyTransaction);
+    });
+    // find current sport energy balance
+    var whereStr = {'cardNumber':req.body.cardNumber};
+    dbo.collection('sportEnergyAccount').find(whereStr).toArray(function(err, sportEnergyAccount_old) {
+      if (err) throw err;
+      console.log(sportEnergyAccount_old[0].energyBalance);
+      // update sport energy balance
+      var updateStr = {$set:{'energyBalance':Number(req.body.energyUpdate)+Number(sportEnergyAccount_old[0].energyBalance), 'updateTime':myDate.toLocaleString( )}};
+      dbo.collection('sportEnergyAccount').updateOne(whereStr, updateStr, function(err, sportEnergyAccount_new) {
         if (err) throw err;
-        res.json({Message: 'Create sport leader successfully'});
+        console.log(sportEnergyAccount_new);
         db.close();
+      });
     });
   });
 };
 
 exports.delete_sportEnergyTransaction = function(req, res) {
-  if (req.query.profile) {
-    console.log('Delete sport leader by profile');
+  if (req.query.cardNumber) {
+    console.log('Delete sport energy transaction by card number');
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-      var dbo = db.db("sportLeaderDB");
-      var whereStr = {'profile':req.query.profile};
-      dbo.collection('sportLeader').deleteOne(whereStr, function(err, sportLeader) {
+      var dbo = db.db('sportEnergyDB');
+      var whereStr = {'cardNumber':req.query.cardNumber};
+      dbo.collection('sportEnergyTransaction').deleteOne(whereStr, function(err, sportEnergyTransaction) {
         if (err) throw err;
-        res.json({Message: 'Delete sport leader by profile successfully'});
+        res.json({Message: 'Delete sport energy account by card number successfully'});
         db.close();
       });
     });
@@ -93,21 +79,21 @@ exports.delete_sportEnergyTransaction = function(req, res) {
 };
 
 exports.update_sportEnergyTransaction = function(req, res) {
-  if (req.query.profile) {
-    console.log('Update sport leader by profile');
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db('sportLeaderDB');
-    var whereStr = {'profile':req.query.profile};
-    var updateStr = {$set:req.body};
-    dbo.collection('sportLeader').updateOne(whereStr, updateStr, function(err, sportLeader) {
-        if (err) throw err;
-        res.json(sportLeader);
-        db.close();
-    });
-});
-  }
-  else {
-    res.json({Message: 'Please provide necessary information to update sport leader.'})
-  }
+//   if (req.query.profile) {
+//     console.log('Update sport leader by profile');
+//     MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db('sportLeaderDB');
+//     var whereStr = {'profile':req.query.profile};
+//     var updateStr = {$set:req.body};
+//     dbo.collection('sportLeader').updateOne(whereStr, updateStr, function(err, sportEnergyTransaction) {
+//         if (err) throw err;
+//         res.json(sportEnergyTransaction);
+//         db.close();
+//     });
+// });
+//   }
+//   else {
+//     res.json({Message: 'Please provide necessary information to update sport leader.'})
+//   }
 };
